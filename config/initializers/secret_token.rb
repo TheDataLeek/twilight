67b1406277ef1ed160204c342c3c2f61d82fc05a
William Farmer <willzfarmer@gmail.com>
2013-11-08 14:39:57 -0700

    INITIAL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twilight::Application.config.secret_key_base = '5add865fa6a32564c5b4aaa488e1cdaa46f1cfe7f2746d46ebc4815e4801336924ed1b9f94d0ae0ce7db419ce8c1d1049c070b15f701996e7d8ade499ac31485'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Twilight::Application.config.session_store :cookie_store, key: '_twilight_session'
