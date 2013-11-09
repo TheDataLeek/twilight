a32a5eb9704a785f1c5a74b34dced1f3da0d2d45
William Farmer <willzfarmer@gmail.com>
2013-11-08 21:46:54 -0700

    Added controller and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Twilight::Application.routes.draw do
+  get "ranking/index"
+
+  root 'ranking#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
