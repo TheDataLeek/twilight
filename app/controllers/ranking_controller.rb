100fdecc892961b3e34829848e5420b07a4993d2
William Farmer <willzfarmer@gmail.com>
2013-11-09 15:27:41 -0700

    Removed pages. Not needed

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,18 +1,4 @@
 class RankingController < ApplicationController
-  attr_accessor :pages
-
-  add_page("Index", "/")
   def index
-  end
-
-  def add_page(pname, purl)
-    new_page      = Page.new
-    new_page.name = pname
-    new_page.url  = purl
-    @pages << new_page
-  end
-
-  class Page
-    attr_accessor :url,:name
   end
 end
