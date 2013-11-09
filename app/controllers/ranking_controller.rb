f0a45ba48be7169f822b4c19d4158f2ce0d94715
William Farmer <willzfarmer@gmail.com>
2013-11-09 00:21:10 -0700

    Added basic views. Figuring stuff out

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,4 +1,28 @@
 class RankingController < ApplicationController
   def index
+    page1      = Page.new
+    page1.name = "test1"
+    page1.url  = "google.com"
+    page2      = Page.new
+    page2.name = "test2"
+    page2.url  = "reddit.com"
+    @pages     = [page1, page2]
+  end
+
+  class Page
+    @name = nil
+    @url = nil
+    def name=(value)
+      @name = value
+    end
+    def name
+      @name
+    end
+    def url=(value)
+      @url= value
+    end
+    def url
+      @url
+    end
   end
 end
