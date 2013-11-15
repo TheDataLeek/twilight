eac07b448c2f22c30b211b9de47919aad3d0e078
William Farmer <willzfarmer@gmail.com>
2013-11-15 15:50:10 -0700

    Watchdog shiny

--- bin/watcher.py
+++ bin/watcher.py
@@ -6,6 +6,10 @@ import watchdog
 import sys
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
+
+from get_user import *
+from rank_user import *
 
 def main():
     open('../log/pyRank.log', 'w').close()  # Testing only. Delete for prod
@@ -13,11 +17,18 @@ def main():
                         level=logging.DEBUG,
                         format='%(levelname)s\t|\t%(asctime)s\t-\t %(message)s')
     logging.info("Program Start")
+    start_watchdog()
 
-    event_handler = LoggingEventHandler()
+
+def start_watchdog():
+    event_handler = FileSystemEventHandler()
     observer      = Observer()
     observer.schedule(event_handler, path='../watch')
     observer.start()
+    log_handler   = LoggingEventHandler()
+    log_observer  = Observer()
+    log_observer.schedule(log_handler, path='../watch')
+    log_observer.start()
     try:
         logging.info("Watching Directory")
         while True:
@@ -25,10 +36,14 @@ def main():
     except KeyboardInterrupt:
         logging.info("Kill message sent. Aborting")
         observer.stop()
+        log_observer.stop()
     except:
         logging.info("Unexpected error: %s" % sys.exc_info()[0])
+        observer.stop()
+        log_observer.stop()
         raise
     observer.join()
+    log_observer.join()
 
 
 if __name__ == "__main__":
