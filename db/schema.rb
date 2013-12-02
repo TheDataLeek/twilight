7a4baa13ecf371aca1a8d34e8daf6bb3c8f54167
William Farmer <willzfarmer@gmail.com>
2013-12-01 21:02:14 -0700

    Added columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131127054228) do
+ActiveRecord::Schema.define(version: 20131202035802) do
 
   create_table "followers", force: true do |t|
     t.integer  "user"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20131127054228) do
     t.integer  "statuses_count"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
+    t.string   "hash"
   end
 
 end
