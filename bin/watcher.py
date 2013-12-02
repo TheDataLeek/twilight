dce28532bcd29b4cf94538fdcc0728b0bb90c4d9
William Farmer <willzfarmer@gmail.com>
2013-12-02 15:42:06 -0700

    Watchdog Event Handler

--- bin/watcher.py
+++ bin/watcher.py
@@ -12,11 +12,13 @@ from watchdog.events import FileSystemEventHandler
 from get_user import *
 from rank_user import *
 
+
 fromaddr    = 'TwilightRanker@gmail.com'
 toaddrs     = ['willzfarmer@gmail.com', 'da007penguin@gmail.com']
 credentials = open('../config/gmail_credentials.txt')
 username    = credentials.readline()[:-1]
 password    = credentials.readline()[:-1]
+
 
 def main():
     open('../log/pyRank.log', 'w').close()  # Testing only. Delete for prod
@@ -28,15 +30,15 @@ def main():
 
 
 def start_watchdog():
-    event_handler = FileSystemEventHandler()
+    event_handler = RankingHandler()
     observer      = Observer()
-    observer.schedule(event_handler, path='../watch')
-    observer.start()
     log_handler   = LoggingEventHandler()
     log_observer  = Observer()
-    log_observer.schedule(log_handler, path='../watch')
-    log_observer.start()
     try:
+        observer.schedule(event_handler, path='../watch')
+        observer.start()
+        log_observer.schedule(log_handler, path='../watch')
+        log_observer.start()
         logging.info("Watching Directory")
         while True:
             time.sleep(1)
@@ -46,19 +48,44 @@ def start_watchdog():
         log_observer.stop()
     except:
         logging.info("Unexpected error: %s" % sys.exc_info()[0])
+
         observer.stop()
         log_observer.stop()
 
-        # Send Email
-        msg = "Unexpected error: %s\nScript Failed. Please log in and restart manually" % sys.exc_info()[0]
-        for receiver in toaddrs:
-            server = smtplib.SMTP('smtp.gmail.com:587')
-            server.starttls()
-            server.login(username,password)
-            server.sendmail(fromaddr, receiver, msg)
-            server.quit()
+        error_message(sys.exc_info()[0])
+
     observer.join()
     log_observer.join()
+
+
+def error_message(error, msg=None):
+    # Send Email
+    if not msg:
+        msg = "Unexpected error: %s\nRanking Script Failed. Please log in and restart manually\n\nEd, this is probably your fault..." % error
+    for receiver in toaddrs:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(username,password)
+        server.sendmail(fromaddr, receiver, msg)
+        server.quit()
+
+
+class RankingHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.modified     = False
+        self.last_checked = time.time()
+        self.time_gap     = None
+
+    def on_any_event(self, event):
+        self.modified = True
+        self.time_gap = time.time() - self.last_checked
+        if self.time_gap >= 5: #900:
+            self.last_checked = time.time()
+            try:
+                users = open('../watch/get_users.txt', 'r').read()
+                print(users)
+            except IOError:
+                error_message(sys.exc_info()[0], msg="Misaligned I/O")
 
 
 if __name__ == "__main__":
