745c73813a937d051e9c6e3b8ba3727947f20adb
William Farmer <willzfarmer@gmail.com>
2013-12-10 13:44:09 -0700

    Decided against destroying

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     def destroy
         User.find(params[:id]).destroy
         flash[:success] = "User destroyed."
-        redirect_to users_url
+        redirect_to root_url
     end
 
     private
