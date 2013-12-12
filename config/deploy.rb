397e3e4ba4765d59dfdaa20b2f52c9471954c8be
William Farmer <willzfarmer@gmail.com>
2013-12-11 19:08:51 -0700

    Production DB

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :deploy_to, '/var/www/twilight'
 set :scm, :git
 set :branch, "master"
 set :user, "william"
-set :use_sudo, true
+set :use_sudo, false
 set :rails_env, "production"
 set :deploy_via, :copy
 set :keep_releases, 5
@@ -33,20 +33,20 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
+        #run "touch #{ current_path }/tmp/restart.txt"
     end
   end
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-       within release_path do
-         execute :rake, 'cache:clear'
-       end
+       # within release_path do
+       #   execute :rake, 'cache:clear'
+       # end
     end
   end
 
+  after :finishing, "deploy:restart"
   after :finishing, 'deploy:cleanup'
 
 end
