c779a4a781349f9cbc9e507a24b14c193b2a9282
William Farmer <willzfarmer@gmail.com>
2013-12-18 11:06:13 -0700

    Working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,5 +8,29 @@ class StaticPagesController < ApplicationController
     def about
         @users = User.all
         @users.sort_by!{|e| -e[:score]}
+
+        jsonfile = {"name"=>"Users",
+                      "children"=>Array.new}
+        @users.each do |u|
+            user =  {"name"=>u.username,
+                     "size"=>2000,
+                     "children"=>Array.new}
+            Followers.where(user:u.userid).each do |f|
+                followuser = User.find_by(userid: f.follows)
+                if followuser.nil?
+                    name = f.follows
+                else
+                    name = followuser.username
+                end
+                user["children"] << {"name"=>name,
+                                     "size"=>2000}
+            end
+            jsonfile['children'] << user
+        end
+
+        File.open("public/network.json","w") do |f|
+            f.write(jsonfile.to_json)
+        end
+
     end
 end
